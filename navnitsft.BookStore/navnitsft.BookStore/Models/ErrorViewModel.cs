e5c49f818f78935b2a57bf1f79ab2c1c1a2c7698
Navneet Kumar <navnitsft@gmail.com>
2023-08-18 12:57:24 +0530

    Created BookStore application

---
+++ navnitsft.BookStore/navnitsft.BookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace navnitsft.BookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
