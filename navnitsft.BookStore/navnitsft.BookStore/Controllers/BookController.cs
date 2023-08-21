61186dafb022cbf7c39adc85567f74bd6f05099d
Navneet Kumar <navnitsft@gmail.com>
2023-08-21 20:30:01 +0530

    Book controller, model & repository created

---
+++ navnitsft.BookStore/navnitsft.BookStore/Controllers/BookController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using navnitsft.BookStore.Models;
+using navnitsft.BookStore.Repository;
+
+namespace navnitsft.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        public List<BookModel> GetAllBooks()
+        {
+            return _bookRepository.GetAllBooks();
+        }
+
+        public List<BookModel> GetBook(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+
+        public List<BookModel> SearchBook(string bookName, string authorName) 
+        {
+            return _bookRepository.SearchBook(bookName, authorName);
+        }
+    }
+}
---
+++ navnitsft.BookStore/navnitsft.BookStore/Repository/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using navnitsft.BookStore.Models;
+
+namespace navnitsft.BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public List<BookModel> GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).ToList();
+        }
+
+        public List<BookModel> SearchBook(string title, string authorName)
+        {
+            return DataSource().Where(x => x.Title==title && x.Author==authorName).ToList();
+        }
+
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel() {Id=1, Title="MVC", Author="Ram"},
+                new BookModel() {Id=2, Title="MVC", Author="Ram"},
+                new BookModel() {Id=3, Title="C#", Author="Shyam"},
+                new BookModel() {Id=4, Title="Java", Author="Mohan"},
+                new BookModel() {Id=5, Title="Php", Author="Mohan"}
+            };
+        }
+        
+    }
+}
